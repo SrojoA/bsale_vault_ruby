3ef569ebdbd2240e3562a95407cdfaea542b021c
SrojoA <srojo@imaginex.cl>
2023-06-17 13:12:25 -0400

    se organiza y simplifica código

--- main.rb
+++ main.rb
@@ -5,7 +5,8 @@ Dotenv.load
 
 def vault_connector
   config = {
-    address: 'http://54.227.105.83:8200' #url de vault
+    address: 'http://54.227.105.83:8200', #url de vault
+    token: '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'
   }
 
   client = Vault::Client.new(config)
@@ -15,51 +16,36 @@ def vault_connector
     puts "No se pudo conectar"
   end
 
-  options = {
-    # token: ENV['git_token']  //usar este metodo
-    token: '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'
-  }
-
-  response = client.logical.write('auth/github/login', options)
+  response = client.logical.write('auth/github/login', config)
   client.token = response.auth.client_token
-
   client
 end
 
 # Método que luego de conectarse a vault retorna los secretos generales
 def general_secrets_by_environment(environment)
   client = vault_connector()
-
   path = "#{environment}/data/general"
-
-  secrets = client.logical.read(path)
-
-  return secrets.data.to_json if secrets
-
-  nil #Si no hay secreto nil
+  secrets = client.logical.read(path).data.to_json
 end
 
 # obtiene y retorna los secretos para un proyecto en específico.
 def secrets_by_project(environment, project)
-  client = vault_connector
-
+  client = vault_connector()
   path = "#{environment}/data/#{project}"
-
-  secrets = client.logical.read(path)
-  return secrets.data if secrets
-
-  nil
+  secrets = client.logical.read(path).data.to_json
 end
 
-#  obtiene y retorna el valor de una key
-def get_secret_value(secrets, key)
-  value = secrets.dig('data', key)
-  return value if value
-
-  puts "Error obtaining secret value: #{key}"
-  nil
-end
+#  obtiene y retorna el valor de una key especifica
+# def get_secret_value(secrets, key)
+#   value = secrets.dig('data', key)
+#   return value if value
+#   puts "Error obtaining secret value: #{key}"
+#   nil
+# end
 
 # Accede a secretos generales
 secrets = general_secrets_by_environment('development')
 puts secrets.to_json
+
+secretsByProject = secrets_by_project('development', 'bquery_api')
+puts secretsByProject
